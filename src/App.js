6c3843e244dd295a44dde268ffb0b05abf3fe4e3
swoorr <furkansahin23@gmail.com>
2021-01-06 03:52:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Body from "./lib/Body";
+
+const user = {
+  first: "Furkan",
+  last: "Sahin"
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {user.first} {user.last}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="container">
+        <Body /> 
+      </div>
     </div>
   );
 }
---
+++ src/lib/Body.js
@@ -0,0 +1,82 @@
+
+import React from 'react';
+import axios from "axios";
+
+
+class Body extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date(), comm: [], comment_name: "", comment_content:""};
+  }
+
+  __getDate(){
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  handleChange(event){
+    this.setState({comment_name:event.target.value})
+  }
+  handleChangeComm(event){
+    this.setState({comment_content:event.target.value})
+  }
+
+
+  __getData = () => {
+    axios.get("https://5fefb9179515c40017974326.mockapi.io/api/comments").then(data => {
+      var selam = data.data;
+      this.setState({
+        comm:selam
+      })
+    })
+  }
+
+  __submit = (e) => {
+    console.log("alert")
+    axios.post("https://5fefb9179515c40017974326.mockapi.io/api/comments", {date:this.state.date.toLocaleTimeString(),name:this.state.comment_name, content:this.state.comment_content})
+    .then(data => {
+      this.__getData();
+    })
+    e.preventDefault();
+  }
+
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.__getDate(),
+      1000
+    );
+
+    this.__getData();
+
+  }
+
+  render() {
+    return (
+      <div className="page__body">
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
+        <h3>Comment form...</h3>
+        <form onSubmit={this.__submit.bind(this)}>
+          <input onChange={this.handleChange.bind(this)} value={this.comment_name} type="text" name="body" placeholder="Name" /><br />
+          <textarea 
+          onChange={this.handleChangeComm.bind(this)}
+          value={this.comment_content}
+          name="content" placeholder="Content"></textarea>
+          <br />
+          <input type="submit" value="Submit comment" /> 
+        </form>
+        
+        <h3>Comments list...</h3>
+        {Object.values(this.state.comm).map(d => 
+        <p className="border" key={d.id}>
+          <b>{d.name} - {d.date} </b><br />
+          {d.content}
+        </p>
+          )}
+      </div>
+    );
+  }
+}
+
+export default Body;
